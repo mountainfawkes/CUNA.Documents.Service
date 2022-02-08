42580d823c9763fb64ff1ac523c73028a2faac25
Jonathan Stull <jonathan.d.stull@gmail.com>
2022-02-07 19:08:31 -0800

    scaffolds models library and payload model

---
+++ CUNA.Documents.Service.Models/Payload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CUNA.Documents.Service.Models
+{
+    public class Payload
+    {
+        public string Body { get; set; }
+        public string Callback { get; set; }
+        public string Detail { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public Guid Id { get; set; }
+    }
+}
