80445fd9f235dc827dcd5a6474e8263e171e2bc8
Jonathan Stull <jonathan.d.stull@gmail.com>
2022-02-08 13:12:14 -0800

    scaffolds new test project, passes test 1

---
+++ CUNA.Documents.Service.Test/ThirdPartyClient.Test.cs
@@ -0,0 +1,30 @@
+﻿using CUNA.Documents.Service.Models;
+using CUNA.Documents.Service.ThirdParty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CUNA.Documents.Service.Tests
+{
+    public class ThirdPartyClientTest
+    {
+        static public Guid id = Guid.NewGuid();
+
+        static Payload payload = new Payload
+        {
+            Body = "This is a sample payload",
+            Id = id,
+            Callback = $"/callback/{id}"
+        };
+
+        [Fact]
+        public async void QueryFail()
+        {
+            var response = await ThirdPartyClient.ServiceRequest(payload);
+            Assert.False(response.IsSuccessStatusCode);
+        }
+    }
+}
