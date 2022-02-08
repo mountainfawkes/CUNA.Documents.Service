e024b22381742e504f627833484f6bbc9dc59a31
Jonathan Stull <jonathan.d.stull@gmail.com>
2022-02-07 21:36:21 -0800

    adds empty stubbed client

---
+++ CUNA.Documents.Service.Client/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CUNA.Documents.Service.Client
+{
+    public class Client
+    {
+        /* Ordinarily, I'd use this opportunity to build a more robust HttpClient instance to build and query the third-party service.
+        We'll be stubbing our calls today. Since we invoke HttpClient requests with a function, I'll use functions here to 
+        stub the client response. For the purpose of this exercise, we should assume that calls would be made to http://example.com. */
+
+        public static ServiceRequest(Payload payload)
+        {
+            
+        }
+    }
+}
