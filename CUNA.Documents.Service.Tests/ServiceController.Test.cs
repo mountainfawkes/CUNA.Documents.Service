d46c6926b0addddb46ee8e0be699b9b84c9f5bf0
Jonathan Stull <jonathan.d.stull@gmail.com>
2022-02-08 12:27:29 -0800

    scaffolds test classes

---
+++ CUNA.Documents.Service.Tests/ServiceController.Test.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CUNA.Documents.Service.Tests
+{
+    public class ServiceControllerTest
+    {
+
+    }
+}
---
+++ CUNA.Documents.Service.Tests/ThirdPartyClient.Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUNA.Documents.Service.Tests
+{
+    public class ThirdPartyClientTest
+    {
+
+    }
+}
